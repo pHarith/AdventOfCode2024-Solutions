013ccc8c316d698dd53fe7df9147431cf1d3e87a
suonseap <seappharith.suon@mail.utoronto.ca>
2026-01-15 01:40:35 -0500

    added a few functions for day 21 sol

--- 21/day21sol.py
+++ 21/day21sol.py
@@ -6,16 +6,29 @@
 # 3. Compute the complexity score of each input 
 # 4. Sum the complexity score and return
 
+UP, DOWN, LEFT, RIGHT = '^', 'v', '<', '>'
+movement_vectors = {UP: (0, -1), RIGHT: (1, 0), DOWN: (0, 1), LEFT: (-1, 0)} 
+
 # Read the input from the input file
 def read_input(input_file):
     with open(input_file, "r") as numeric_file:
         return [line.strip() for line in numeric_file]
 
 def init_directional_keypad():
-    pass
+    """
+    Returns a dictionary that represents the directional keypad
+    """
+    return {(0, 0): '7', (0, 1): '8', (0, 2): '9',
+            (1, 0): '4', (1, 1): '5', (1, 2): '6',
+            (2, 0): '1', (2, 1): '2', (2, 2): '3',
+            (3, 0): None, (3, 1): '0', (3, 2): 'A'}
 
 def init_numeric_keypad():
-    pass
+    """
+    Returns a dictionary that represents the numeric keypad
+    """
+    return {(0, 0): None, (0, 1): UP, (0, 2): 'A',
+            (1, 0): LEFT, (1, 1): DOWN, (1, 2): RIGHT}
 
 def compute_complexity_score(code_numeric, sequence_length):
     return code_numeric * sequence_length
