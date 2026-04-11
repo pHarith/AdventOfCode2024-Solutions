79735dc56c8cb0e4bfd8c006ef1871a95e0daf4d
suonseap <seappharith.suon@mail.utoronto.ca>
2026-04-11 16:51:46 -0400

    solved part 1 of day 22, added partial functions for part 2

--- 22/day22sol.py
+++ 22/day22sol.py
@@ -1,26 +1,59 @@
 # Solution to day 22: Monkey Market
 
+PRUNE_CONST = 16777216
 
-def mix_number(num):
+REPEAT_CONST = 2000
+
+def mix(num, to_mix):
     """
     Mix the numbers according to the problem's description.
     """        
-    pass
+    return num ^ to_mix
 
-def prune_number(num):
+def prune(num):
     """
     Prune the number <num> according to the problem's description.
     """        
-    pass
+    return num % PRUNE_CONST
 
-def read_input():
-    pass
+def evolve(num):
+    """
+    Evolves the number <num> according to the problem's description.
+    """        
+    first = prune(mix(num, num * 64))
+    second = prune(mix(first, first // 32))
+    final = prune(mix(second, second * 2048))
+    return final
+
+def read_input(file):
+    with open(file, "r") as num_file:
+        return [int(line.strip()) for line in num_file]
 
 def solve(input_file):
     """
     Produce the solution to the day 22 problem - Monkey Market
     """
-    return
+    secret_nums = read_input(input_file)
+    for i in range(len(secret_nums)):
+        for _ in range(REPEAT_CONST):
+            secret_nums[i] = evolve(secret_nums[i])
+
+    return sum(secret_nums)
+
+def solve_part2(input_file):
+    """
+    Produce the solution to part 2 of the day 22 problem - Monkey Market
+    """
+    secret_nums = read_input(input_file)
+    for i in range(len(secret_nums)):
+        for _ in range(REPEAT_CONST):
+            secret_nums[i] = evolve(secret_nums[i])
+
+            # Extract the last digit
+            last_digit = secret_nums[i] % 10
+
+
+    return sum(secret_nums)
 
 
 if __name__ == "__main__":
