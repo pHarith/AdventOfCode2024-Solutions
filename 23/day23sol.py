8a88e32f36df8104e73fefba00f187bfab64892a
suonseap <seappharith.suon@mail.utoronto.ca>
2026-04-16 01:55:06 -0400

    added file reading function to day 23 solution

--- 23/day23sol.py
+++ 23/day23sol.py
@@ -1,17 +1,34 @@
 # Solution to day 23: LAN Party
 #### SUMMARY OF TASKS ####
-# 1. 
+# 1. Read the input file into a set of pairs
+# 2. TODO: 
+# 3. 
 
 
         
-
+def read_network_map(input_file):
+    """
+    Given input text file of <com-a>-<com-b> lines, return a set of unique computers
+    and a set of unique connections.
+    """
+    coms = set()
+    connections = set()
+    with open(input_file, "r") as network_map:
+        for pair in network_map:
+            com_1, com_2 = pair.strip().split('-')
+            connections.add((com_1, com_2))
+            coms.add(com_1)
+            coms.add(com_2)
+    return coms, connections
 
 
 def solve(input_file):
     """
     Produce the solution to the day 23 problem - LAN Party
     """
-    return
+    computers, connections = read_network_map(input_file)
+
+    return computers, connections
 
 
 if __name__ == "__main__":
