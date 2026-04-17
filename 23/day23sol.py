f9c8dd734800f5d3bada77de547d1035cd6889d1
suonseap <seappharith.suon@mail.utoronto.ca>
2026-04-16 20:50:57 -0400

    solved part 1 of day 23

--- 23/day23sol.py
+++ 23/day23sol.py
@@ -4,6 +4,7 @@
 # 2. TODO: 
 # 3. 
 
+from itertools import combinations
 
         
 def read_network_map(input_file):
@@ -16,7 +17,7 @@ def read_network_map(input_file):
     with open(input_file, "r") as network_map:
         for pair in network_map:
             com_1, com_2 = pair.strip().split('-')
-            connections.add((com_1, com_2))
+            connections.add((min(com_1, com_2), max(com_1, com_2)))
             coms.add(com_1)
             coms.add(com_2)
     return coms, connections
@@ -28,7 +29,16 @@ def solve(input_file):
     """
     computers, connections = read_network_map(input_file)
 
-    return computers, connections
+    valid_trios = set()
+
+    for trio in combinations(computers, 3):
+        # Check that any two pairs in a trio is connected
+        if all(tuple(sorted(pair)) in connections for pair in combinations(trio, 2)):
+            for comp in trio:
+                if comp.startswith('t'):    # Check for computers that starts with 't'
+                    valid_trios.add(trio)
+
+    return len(valid_trios)
 
 
 if __name__ == "__main__":
