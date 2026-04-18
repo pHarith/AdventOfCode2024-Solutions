606ec77d2f048e12f83d1f010683d1c14dbc57f7
suonseap <seappharith.suon@mail.utoronto.ca>
2026-04-18 15:10:26 -0400

    added skeleton code to part 2, updated todos and problem.txt

--- 23/day23sol.py
+++ 23/day23sol.py
@@ -40,8 +40,44 @@ def solve(input_file):
 
     return len(valid_trios)
 
+def solve_part2(input_file)
+    """
+    Produce the solution to part 2 of day 23 problem - LAN Party
+    """
+    computers, connections = read_network_map(input_file)
+    
+
+    # TODO: Implement the bron-kerbosch algorithm to build the largest LAN party
+    def bron_kerbosch(R=set(), P=computers, X=set(), connections=connections, largest):
+        """ 
+        Build <largest> recursively using <connections> where:
+        R: the maximal clique, the largest clique in.
+        P: the list of all possible candidates - computers that we have not explored.
+        X: the list of explored candidates - computers that are confirmed as part of or not part of 
+        the current clique, but if our maximal clique changes, they can be re-explored.
+        """
+        # Base case: 
+        # Check if R is maximal - R is maximal if we can't add anymore from P nor X
+        if len(P) == 0 and len(X) == 0:
+            largest = R
+
+        # Recursive case:
+        # TODO: work on recursive condition
+    
+    
+        return largest
+
+
+    password = ""
+    # TODO: Form the password to the largest LAN party by:
+    # 1. arrange all computers returned from the helper function in alphabetical order
+    # 2. concaternate into a string, separated by commas
+
+    return password
 
 if __name__ == "__main__":
     input = 'input.txt'
     # input = 'test.txt'
     print(solve(input))
+
+    print(solve_part2(input))
