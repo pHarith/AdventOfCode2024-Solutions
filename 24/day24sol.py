f9b3cdbff1e1a9e1e09217be90f0f34d1214a951
suonseap <seappharith.suon@mail.utoronto.ca>
2026-04-23 00:45:20 -0400

    added file i/o helper function to day 24 sol

--- 24/day24sol.py
+++ 24/day24sol.py
@@ -2,7 +2,30 @@
 
 
 def read_wire_config(input_file):
-    pass
+    """
+    Return two dictionaries from reading the input file:
+    - First dictionary stores the name of wires as keys and their values are 0 or 1
+    - Second dictionary stores the name of gates as keys and their values are tuples of 3 wires, (input1, input2, output)
+    """
+    wires = {}
+    gates = {}
+
+    with open(input_file, "r") as wire_config:
+        wire_info, gates_info = wire_config.read().split('\n\n') # split the file into two sections
+
+        for line in wire_info.split('\n'):
+            wire, value = line.strip().split(':')
+            wires[wire] = int(value)
+
+        for line in gates_info.split('\n'):
+            inputs, output = line.strip().split('->')
+
+            input1, gate, input2 = inputs.strip().split(' ')
+            if gate not in gates:
+                gates[gate] = [(input1, input2, output)]
+            else:
+                gates[gate].append((input1, input2, output))
+    return wires, gates
 
 
 def read_z_binary():
@@ -19,10 +42,15 @@ def solve(input_file):
     """
     Produce the solution to the day 24 problem - Crossed Wires
     """
-    return
+    # Read the wire values and logic gates
+    wires, gates = read_wire_config(input_file)
+
+
+    return 
 
 
 if __name__ == "__main__":
     input = 'input.txt'
-    # input = 'test.txt'
+    input = 'test.txt'
+    # input = 'smalltest.txt'
     print(solve(input))
