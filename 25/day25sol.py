2d75a9e52ad18ff1485c112a39807ef0830aa0c7
suonseap <seappharith.suon@mail.utoronto.ca>
2026-05-27 23:47:57 -0400

    added skeleton of helpers for day 25 solution part 1

--- 25/day25sol.py
+++ 25/day25sol.py
@@ -1,9 +1,36 @@
 # Solution to day 25: Code Chronicle
-#### SUMMARY OF TASKS ####
-# 1. 
+#### SUMMARY OF TASKS (part 1) ####
+# 1. Read the schematics from the input file, likely a matrix, separating locks and keys
+# 2. Convert the matrix into a list of heights, and group them into 2 sets: locks, keys.
+# 3. Attempt matching the key, locks by checking if: 
+# height_of_key_col + height_of_lock_col <= height_of_available_col
+# 4. Return the number of key-lock pairs that fit
 
 
-        
+# Helper functions for part 1
+def read_key_lock_schematics(input):
+    keys, locks = set(), set()
+    with open(input, "r") as schematics:
+        # Split the file into strings of schematics
+        key_locks_str = schematics.read().split('\n\n')
+
+        # Convert each string item into a matrix
+        for item_str in key_locks_str:
+            mtx = item_str.strip().split('\n')
+            if item_str[0] == '#':   # is a lock
+                pass
+            elif item_str[0] == '.': # is a key
+                pass
+            else:
+                raise ValueError("Expected '.' or '#' characters. File Error")
+
+    return keys, locks   
+
+
+def convert_mtx_into_number_lst(mtx):
+    count = []
+    # TODO: Convert each matrix into a list of numbers based on numbers of # in each column
+    return count
 
 
 
