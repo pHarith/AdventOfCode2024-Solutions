1e1a04311d2c53348c67d0e0475df7add7575170
suonseap <seappharith.suon@mail.utoronto.ca>
2026-05-30 14:57:02 -0400

    cleaned up solution and comments

--- 25/day25sol.py
+++ 25/day25sol.py
@@ -13,8 +13,6 @@ def read_key_lock_schematics(input):
     with open(input, "r") as schematics:
         # Split the file into strings of schematics
         key_locks_str = schematics.read().split('\n\n')
-        
-        print(f"{key_locks_str}")
 
         key_locks_lst = [item_str.strip().split('\n') for item_str in key_locks_str]
 
@@ -23,22 +21,33 @@ def read_key_lock_schematics(input):
             num_rows, num_cols = len(mtx), len(mtx[0])
             count_mtx = convert_mtx_into_height_lst(mtx, num_cols)
             if mtx[0][0] == '#':   # is a lock
-                #locks.append(mtx)
-                locks.append(count_mtx)
+                locks.append(mtx)
             elif mtx[0][0] == '.': # is a key
-                #keys.append(mtx)
-                keys.append(count_mtx)
+                keys.append(mtx)
             else:
                 raise ValueError("Expected '.' or '#' characters. Invalid File.")
-    print(f'{num_rows = } , {num_cols = }')
-    print(f'{keys = }')
-    print(f'{locks = }')
-    return num_rows, num_cols, keys, locks   
+    return num_rows, num_cols, keys, locks
+
+def build_height_lst(num_cols, keys, locks):
+    """
+    Build a nested list containing the number of '#' in each columns of keys and locks
+    matrices. 
+    Return a nested list for each.
+    """
+    count_keys, count_locks = [], []
+
+    for mtx in keys:
+        count_keys.append(convert_mtx_into_height_lst(mtx, num_cols))
+
+    for mtx in locks:
+        count_locks.append(convert_mtx_into_height_lst(mtx, num_cols))
+
+    return count_locks, count_keys
 
 
 def convert_mtx_into_height_lst(mtx, num_cols):
     count = [0] * num_cols
-    # TODO: Convert each matrix into a list of numbers based on numbers of # in each column
+    
     for row in mtx:
         for j in range(num_cols):
             if row[j] == '#':
@@ -48,31 +57,31 @@ def convert_mtx_into_height_lst(mtx, num_cols):
     return count
 
 
-
 def solve(input_file):
     """
     Produce the solution to the day 25 problem - Code Chronicle
     """
-    
     valid_combos = []
 
     num_rows, num_cols, keys, locks = read_key_lock_schematics(input)
 
-    for key in keys:
-        for lock in locks:
+    count_keys, count_locks = build_height_lst(num_cols, keys, locks)
+
+    for count_key in count_keys:
+        for count_lock in count_locks:
             
             invalid = False
 
             for i in range(num_cols):
                 if invalid:
                     break
-                elif key[i] + lock[i] > num_rows:
+                elif count_key[i] + count_lock[i] > num_rows:
                     invalid = True
                 
             if not invalid:
-                valid_combos.append((keys, locks))
+                valid_combos.append((count_keys, count_locks))
 
-    return valid_combos, len(valid_combos)
+    return len(valid_combos)
 
 
 if __name__ == "__main__":
